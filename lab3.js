bdddf8ec63baa83902e8e1f202b99d39d2ce918a
A Newcomer <30941638+a-newcomer@users.noreply.github.com>
2018-07-20 00:57:36 -0700

    Add files via upload

---
+++ lab3.js
@@ -0,0 +1,6 @@
+//console.log( document.getElementById("canvas").height);
+console.log( "working");
+window.addEventListener("onload", testHeight, false);
+function testHeight(){
+    window.innerHeight;
+}
